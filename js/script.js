afd4c67efbab5e81f612aa245e46094a4a8bffc4
Vika <bika0724@gmail.com>
2022-04-26 12:28:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// console.log("arr"+"- object")
+// console.log(4+"5")
+
+// let incr = 10,
+//     decr = 10;
+//
+// // ++incr ; // инкремент
+// // --decr ; // декремент
+//
+// console.log(++incr);
+// console.log(--decr);
+//
+// console.log (5%2)
+
+console.log (2+2*2 !== '6')
+
+// const isChecked = true,
+//       isClose = false;
+//
+// console.log (isChecked && !isClose);
