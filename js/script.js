f1d5eaed847748a31226569e9b49f741f74cffaf
Vika <bika0724@gmail.com>
2022-04-28 09:52:10 +0300

    Оптимизировала условия при помощи циклов Поставила ограничения по вводу значений в строку

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
 "use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-let personalMovieDB = {
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres:[],
+    genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    с = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+for (let i=0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[с] = d;
+    if (a !=null && b !=null && a !='' && b !='' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-console.log(personalMovieDB);
+if (personalMovieDB.count <10) {
+    console.log("Просмотрено мало фильмов");
+} else if ( personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB)
